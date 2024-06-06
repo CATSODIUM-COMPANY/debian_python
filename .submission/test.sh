ef96438ede7d841993368f8adb0e964c608e8707
Thitiwat Kosolpattanadurong <167766536+CATISNOTSODIUM@users.noreply.github.com>
2024-06-06 11:10:28 +0700

    submitted by CATISNOTSODIUM

--- .submission/test.sh
+++ .submission/test.sh
@@ -1,1 +1,12 @@
-Hi
+my_file=$(cat task01_fibonacci.py | base64)
+echo $my_file
+
+
+
+curl -L \
+  -X PUT \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $accessToken" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/CATSODIUM-COMPANY/debian_python/contents/.submission/hello.py\
+  -d "{\"message\":\"my commit message\", \"committer\":{\"name\":\"Monalisa Octocat\",\"email\":\"octocat@github.com\"},\"content\":\"$my_file\"}"
